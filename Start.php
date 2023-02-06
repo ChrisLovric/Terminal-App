88dfd27746ef372fd084177df45c6a2b904bb459
ChrisLovric <klovric991@gmail.com>
2023-02-06 20:29:11 +0100

    dodavanje podizbornika

--- Start.php
+++ Start.php
@@ -49,16 +49,16 @@ class Start{
                 $this->KupacIzbornik();
                 break;
             case 2:
-                //$this->PlacanjeIzbornik();
+                $this->PlacanjeIzbornik();
                 break;
             case 3:
-                //$this->NarudzbaIzbornik();
+                $this->NarudzbaIzbornik();
                 break;
             case 4:
-                //$this->ProizvodIzbornik();
+                $this->ProizvodIzbornik();
                 break;
             case 5:
-                //$this->DetaljiNarudzbe();
+                $this->DetaljiNarudzbe();
                 break;
             case 6:
                 echo 'Vidimo se opet!' . PHP_EOL;
@@ -71,11 +71,83 @@ class Start{
     private function KupacIzbornik(){
         echo 'Kupac' . PHP_EOL;
         echo '1. Pregled kupaca' . PHP_EOL;
-        echo '1. Unos novog kupca' . PHP_EOL;
-        echo '1. Promjena postojećeg kupca' . PHP_EOL;
-        echo '1. Brisanje postojećeg kupca' . PHP_EOL;
-        echo '1. Povratak na glavni izbornik' . PHP_EOL;
-        //$this->odabirOpcijeKupac();
+        echo '2. Unos novog kupca' . PHP_EOL;
+        echo '3. Promjena postojećeg kupca' . PHP_EOL;
+        echo '4. Brisanje postojećeg kupca' . PHP_EOL;
+        echo '5. Povratak na glavni izbornik' . PHP_EOL;
+        $this->odabirOpcijeKupac();
+    }
+
+    private function odabirOpcijeKupac(){
+        switch(Pomocno::rasponBroja('Odaberite opciju: ',1,5)){
+            case 1:
+                //$this->pregledKupaca();
+                break;
+            case 2:
+                //$this->unosNovogKupca();
+                break;
+            case 3:
+                if(count($this->kupac)===0){
+                    echo 'Nema kupaca u aplikaciji' . PHP_EOL;
+                    $this->KupacIzbornik();
+                }else{
+                    //$this->promjenaKupca();
+                }
+                break;
+            case 4:
+                if(count($this->kupac)===0){
+                    echo 'Nema kupaca u aplikaciji' . PHP_EOL;
+                    $this->KupacIzbornik();
+                }else{
+                    //$this->brisanjeKupca();
+                }
+                break;
+            case 5:
+                $this->glavniIzbornik();
+                break;
+            default:
+                $this->KupacIzbornik();
+        }
+    }
+
+    private function PlacanjeIzbornik(){
+        echo 'Plaćanje' . PHP_EOL;
+        echo '1. Vrsta plaćanja' . PHP_EOL;
+        echo '2. Unos nove vrste plaćanja' . PHP_EOL;
+        echo '3. Promjena postojećih vrsta plaćanja' . PHP_EOL;
+        echo '4. Brisanje postojećih vrsta plaćanja' . PHP_EOL;
+        echo '5. Povratak na glavni izbornik' . PHP_EOL;
+        //$this->odabirOpcijePlacanje();
+    }
+
+    private function NarudzbaIzbornik(){
+        echo 'Narudžba' . PHP_EOL;
+        echo '1. Pregled narudžbi' . PHP_EOL;
+        echo '2. Unos nove narudžbe' . PHP_EOL;
+        echo '3. Promjena postojećih narudžbi' . PHP_EOL;
+        echo '4. Brisanje postojećih narudžbi' . PHP_EOL;
+        echo '5. Povratak na glavni izbornik' . PHP_EOL;
+        //$this->odabirOpcijeNarudzba();
+    }
+
+    private function ProizvodIzbornik(){
+        echo 'Proizvod' . PHP_EOL;
+        echo '1. Pregled proizvoda' . PHP_EOL;
+        echo '2. Unos novog proizvoda' . PHP_EOL;
+        echo '3. Promjena postojećeg proizvoda' . PHP_EOL;
+        echo '4. Brisanje postojećeg proizvoda' . PHP_EOL;
+        echo '5. Povratak na glavni izbornik' . PHP_EOL;
+        //$this->odabirOpcijeProizvod();
+    }
+
+    private function DetaljiNarudzbe(){
+        echo 'Detalji narudžbe' . PHP_EOL;
+        echo '1. Detalji narudžbe' . PHP_EOL;
+        echo '2. Unos novih detalja narudžbe' . PHP_EOL;
+        echo '3. Promjena postojećih detalja narudžbe' . PHP_EOL;
+        echo '4. Brisanje postojećih detalja narudžbe' . PHP_EOL;
+        echo '5. Povratak na glavni izbornik' . PHP_EOL;
+        //$this->odabirOpcijeDetaljiNarudzbe();
     }
 }
 
